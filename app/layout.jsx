cea152113e7b0035c92d11e12fcbfec371b0a687
ProAir-Climate-Change <contact@proairuk.co.uk>
2026-03-16 18:43:02 +0000

    Create layout.jsx

---
+++ app/layout.jsx
@@ -0,0 +1,10 @@
+ page.jsx doesn't have a root layout. To fix this error, make sure every page has a root layout.
+> Build error occurred
+Error: Jest worker encountered 1 child process exceptions, exceeding retry limit
+    at ChildProcessWorker.initialize (/vercel/path0/node_modules/next/dist/compiled/jest-worker/index.js:1:11580)
+    at ChildProcessWorker._onExit (/vercel/path0/node_modules/next/dist/compiled/jest-worker/index.js:1:12545)
+    at ChildProcess.emit (node:events:520:35)
+    at ChildProcess._handle.onexit (node:internal/child_process:294:12) {
+  type: 'WorkerError'
+}
+Error: Command "npm run build" exited with 1
