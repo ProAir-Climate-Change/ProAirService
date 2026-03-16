7db7ff50f6e725598d92d8ae59a908c631d9c3e1
ProAir-Climate-Change <contact@proairuk.co.uk>
2026-03-16 18:45:49 +0000

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,10 +1,13 @@
- page.jsx doesn't have a root layout. To fix this error, make sure every page has a root layout.
-> Build error occurred
-Error: Jest worker encountered 1 child process exceptions, exceeding retry limit
-    at ChildProcessWorker.initialize (/vercel/path0/node_modules/next/dist/compiled/jest-worker/index.js:1:11580)
-    at ChildProcessWorker._onExit (/vercel/path0/node_modules/next/dist/compiled/jest-worker/index.js:1:12545)
-    at ChildProcess.emit (node:events:520:35)
-    at ChildProcess._handle.onexit (node:internal/child_process:294:12) {
-  type: 'WorkerError'
+export const metadata = {
+  title: "ProAir Service Tool"
 }
-Error: Command "npm run build" exited with 1
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body style={{ margin: 0 }}>
+        {children}
+      </body>
+    </html>
+  )
+}
